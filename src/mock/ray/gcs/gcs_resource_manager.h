7ac0f9dbf3a91fafcbbeb7e2340f302df00eaa8c
Steve Alexander <sca@anyscale.com>
2026-07-20 17:58:28 +0000

    [core] add storage intf to ClusterResourceManager
    - this is currently a NOP in the raylet
    
    Signed-off-by: Steve Alexander <sca@anyscale.com>

--- src/mock/ray/gcs/gcs_resource_manager.h
+++ src/mock/ray/gcs/gcs_resource_manager.h
@@ -22,14 +22,18 @@
 #include "ray/observability/fake_ray_event_recorder.h"
 #include "ray/pubsub/fake_publisher.h"
 #include "ray/pubsub/gcs_publisher.h"
+#include "ray/raylet/scheduling/raylet_cluster_resource_storage.h"
 #include "ray/util/clock.h"
 
 namespace ray {
 namespace gcs {
 
 static instrumented_io_context __mock_io_context_;
+static std::unique_ptr<ray::raylet::RayletClusterResourceStorage>
+    __mock_cluster_resource_storage_;
 static ClusterResourceManager __mock_cluster_resource_manager_(
-    PeriodicalRunner::Create(__mock_io_context_));
+    PeriodicalRunner::Create(__mock_io_context_),
+    *__mock_cluster_resource_storage_.get());
 static observability::FakeRayEventRecorder __mock_ray_event_recorder_;
 static Clock __mock_clock_;
 static pubsub::ObservabilityPublisher *__mock_observability_publisher() {
